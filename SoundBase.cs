107d92f280864561c004c9fa474626f354cccd42
zhaoguanghui <zhaoguanghui@shinezone.com>
2019-07-17 13:31:54 +0800

    add 7

---
+++ SoundBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PetFamily
+{
+    public class SoundBase : MonoBehaviour
+    {
+        public void PlaySound(int soundID)
+        {
+            SoundMgr.Singleton.Play2DSound(soundID);
+        }
+    }
+}
